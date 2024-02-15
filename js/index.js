778976f00810a39140afdf3577df6806d33c81b2
nhnaahid <nhnahid.iu@gmail.com>
2024-02-15 15:39:28 +0600

    Complete Project without Responsive

--- js/index.js
+++ js/index.js
@@ -4,8 +4,51 @@ function gameLoop() {
     currentAlphabet.innerText = alphabet;
     setBgColorById(alphabet);
 }
+
 function play() {
     hideElementById('home');
+    hideElementById('score');
     showElementById('play-ground');
+
+    setElementValueById('current-score', 0);
+    setElementValueById('current-life', 3);
+
     gameLoop();
 }
+
+function gameOver(value) {
+    hideElementById('play-ground');
+    showElementById('score');
+
+    const finalScore = getElementValueIntById('current-score');
+    setElementValueById('final-score', finalScore);
+
+    const currentAlphabet = getElementValueTextById('current-alphabet');
+    removeBgColorById(currentAlphabet);
+
+}
+
+function handleKeyboardPress(event) {
+    const userPressedKey = event.key;
+    if (userPressedKey === 'Escape')
+        gameOver();
+    const currentAlphabet = document.getElementById('current-alphabet');
+    const expectedKey = currentAlphabet.innerText.toLowerCase();
+    if (userPressedKey === expectedKey) {
+        const currentScore = getElementValueIntById('current-score');
+        const newScore = currentScore + 1;
+        setElementValueById('current-score', newScore);
+        removeBgColorById(userPressedKey);
+        gameLoop();
+    }
+    else {
+        const currentLife = getElementValueIntById('current-life');
+        const newLife = currentLife - 1;
+        setElementValueById('current-life', newLife);
+        if (newLife === 0) {
+            gameOver();
+        }
+    }
+
+}
+document.addEventListener('keyup', handleKeyboardPress);
--- js/utility.js
+++ js/utility.js
@@ -13,9 +13,28 @@ function setBgColorById(elementId) {
     element.classList.add('bg-orange-400');
 }
 
+function removeBgColorById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
+
 function getRandomAlphabet() {
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
     const alphabets = alphabetString.split('');
     const index = Math.round(Math.random() * 25);
     return alphabets[index];
+}
+function getElementValueIntById(elementId) {
+    const element = document.getElementById(elementId);
+    const elementValueInt = parseInt(element.innerText);
+    return elementValueInt;
+}
+function getElementValueTextById(elementId) {
+    const element = document.getElementById(elementId);
+    const elementValueInt = element.innerText;
+    return elementValueInt;
+}
+function setElementValueById(elementId, value) {
+    const element = document.getElementById(elementId);
+    element.innerText = value;
 }
